56d815d48366834d50dd5bb6a8d89491828da8cc
Sami Salih İbrahimbas <info@ssibrahimbas.com>
2022-11-24 19:05:09 +0300

    feat #16: cache skeleton initalized with mapper, repo and service

--- src/domains/metric/metric.mapper.ts
+++ src/domains/metric/metric.mapper.ts
@@ -1,17 +1,23 @@
 import { MetricSchema } from "./metric.schema.ts";
 export const useMetricMapper = () => {
-  const mapCsvToMetricSchema = (csv: any[]): MetricSchema[] => {
+  const mapCsvToMetricSchema = (
+    csv: any[],
+    eachCallback: (schema: MetricSchema) => Promise<unknown>
+  ): Promise<MetricSchema[]> => {
+    const promises: Promise<unknown>[] = [];
     const [header, ...rows] = csv;
-    return rows.map((row: Array<any>) => {
-      return row.reduce((acc, value, index) => {
+    const list = rows.map((row: Array<any>) =>
+      row.reduce((acc, value, index) => {
         if (MetricParsers[header[index]]) {
           acc[header[index]] = MetricParsers[header[index]](value);
         } else {
           acc[header[index]] = value;
         }
+        promises.push(eachCallback(acc));
         return acc;
-      }, {});
-    });
+      }, {})
+    );
+    return Promise.all(promises).then(() => list);
   };
 
   return {
--- src/domains/metric/metric.repo.ts
+++ src/domains/metric/metric.repo.ts
@@ -3,5 +3,17 @@ import { useDatabase } from "../../database/mongo.ts";
 
 export const useMetricRepository = () => {
   const metricCollection = useDatabase().collection<MetricSchema>("metrics");
-  return {};
+
+  const deleteAllMetrics = async () => {
+    await metricCollection.deleteMany({});
+  };
+
+  const insertMetric = async (metric: MetricSchema) => {
+    await metricCollection.insertOne(metric);
+  };
+
+  return {
+    deleteAllMetrics,
+    insertMetric,
+  };
 };
