cec9894bce86bd6586fbb91d91156196cd43d0cf
Sami Salih İbrahimbas <info@ssibrahimbas.com>
2022-11-24 23:26:54 +0300

    repair #23: fix router

--- src/domains/metric/metric.router.ts
+++ src/domains/metric/metric.router.ts
@@ -1,6 +1,11 @@
+import {
+  sendBadRequest,
+  sendJsonResponse,
+} from "../../utils/router-context.ts";
+
+import { MetricMessages } from "./metric.messages.ts";
 import { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { isMetric } from "./metric.types.ts";
-import { sendJsonResponse } from "../../utils/router-context.ts";
 import { useMetricService } from "./metric.service.ts";
 import { useMetricValidator } from "./metric.validator.ts";
 
@@ -8,25 +13,16 @@ export const registerMetricRoutes = (router: Router) => {
   const metricService = useMetricService();
   const metricValidator = useMetricValidator();
 
-  router.get("/metrics", async (ctx) => {
+  router.use(async (ctx, next) => {
     const cached = await metricService.checkCache();
-    if (!cached)
-      return sendJsonResponse(
-        ctx,
-        {
-          success: false,
-          message: "Data is now caching. Please retry a few seconds later",
-        },
-        400
-      );
+    if (!cached) return sendBadRequest(ctx, MetricMessages.caching);
+    return next();
+  });
+
+  router.get("/metrics", async (ctx) => {
     const params = new URL(ctx.request.url).searchParams;
     const metric = params.get("id");
-    if (!isMetric(metric))
-      return sendJsonResponse(
-        ctx,
-        { success: false, message: "Invalid metric" },
-        400
-      );
+    if (!isMetric(metric)) return sendBadRequest(ctx, MetricMessages.invalid);
     const validation = metricValidator.filterByMetric(metric, params);
     if (!validation.success) return sendJsonResponse(ctx, validation, 400);
     sendJsonResponse(
--- src/utils/router-context.ts
+++ src/utils/router-context.ts
@@ -1,5 +1,6 @@
 import type { Context } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import type { RouterContext } from "https://deno.land/x/oak@v11.1.0/router.ts";
+import { failure } from "./response.ts";
 
 export const sendJsonResponse = <B = any>(
   ctx: RouterContext<any, any, any> | Context,
@@ -10,4 +11,11 @@ export const sendJsonResponse = <B = any>(
   ctx.response.body =
     typeof body === "string" ? body : JSON.stringify(body, null, 2);
   ctx.response.type = "application/json";
+};
+
+export const sendBadRequest = (
+  ctx: RouterContext<any, any, any> | Context,
+  msg: string
+) => {
+  sendJsonResponse(ctx, failure(msg), 400);
 };
